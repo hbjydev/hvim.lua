5d4b54a5466fc6c1e42c23ef72ccf11dbbfa784c
Hayden Young <hyoung@acquired.com>
2025-08-12 15:18:33 +0100

    feat: mcphub

--- lua/hvim/plugins/copilot.lua
+++ lua/hvim/plugins/copilot.lua
@@ -4,9 +4,24 @@ return {
     enabled = not Hvim.is_mini() and not Hvim.is_vscode(),
     cmd = 'Copilot',
     event = 'InsertEnter',
-    config = function()
-      require('copilot').setup({})
-    end,
+    opts = {},
+  },
+
+  {
+    'ravitemer/mcphub.nvim',
+    enabled = not Hvim.is_mini() and not Hvim.is_vscode(),
+    dependencies = { "nvim-lua/plenary.nvim" },
+    build = 'bun install --global mcp-hub@latest',
+    opts = {
+      extensions = {
+        copilotchat = {
+          enabled = true,
+          convert_tools_to_functions = true,
+          convert_resources_to_functions = true,
+          add_mcp_prefix = false,
+        },
+      },
+    },
   },
 
   {
