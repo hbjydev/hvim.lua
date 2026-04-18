920d641f29cbf30e6eaeacb85861a0c6f4fbea90
Hayden Young <hayden@hayden.moe>
2026-04-18 19:50:40 +0100

    feat: new vim ui

--- lua/hvim/plugins/ui.lua
+++ lua/hvim/plugins/ui.lua
@@ -1,35 +1,35 @@
 return {
-  {
-    'folke/noice.nvim',
-    enabled = not Hvim.is_vscode(),
-    opts = {
-      lsp = {
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true,
-        },
-      },
-
-      routes = {},
-
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-      },
-    },
-
-    config = function(_, opts)
-      -- HACK: noice shows messages from before it was enabled,
-      -- but this is not ideal when Lazy is installing plugins,
-      -- so clear the messages in this case.
-      if vim.o.filetype == 'lazy' then
-        vim.cmd([[messages clear]])
-      end
-      require('noice').setup(opts)
-    end,
-  },
+  -- {
+  --   'folke/noice.nvim',
+  --   enabled = not Hvim.is_vscode(),
+  --   opts = {
+  --     lsp = {
+  --       override = {
+  --         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+  --         ['vim.lsp.util.stylize_markdown'] = true,
+  --         ['cmp.entry.get_documentation'] = true,
+  --       },
+  --     },
+  --
+  --     routes = {},
+  --
+  --     presets = {
+  --       bottom_search = true,
+  --       command_palette = true,
+  --       long_message_to_split = true,
+  --     },
+  --   },
+  --
+  --   config = function(_, opts)
+  --     -- HACK: noice shows messages from before it was enabled,
+  --     -- but this is not ideal when Lazy is installing plugins,
+  --     -- so clear the messages in this case.
+  --     if vim.o.filetype == 'lazy' then
+  --       vim.cmd([[messages clear]])
+  --     end
+  --     require('noice').setup(opts)
+  --   end,
+  -- },
 
   {
     'nvim-lualine/lualine.nvim',
